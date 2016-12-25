96d68875c81c2316b4e77082de7535ae29a491ca
aqi <andy.qi@activenetwork.com>
2016-12-25 15:45:21 +0800

    rails routes

--- rails.rb
+++ rails.rb
@@ -33,7 +33,6 @@ module Rails
       self.class.endpoint || routes
     end
     def routes
-      return 'routes'
       @routes ||= ActionDispatch::Routing::RouteSet.new_with_config(config)
       @routes
     end
@@ -113,9 +112,21 @@ module ActionDispatch
   class Request
     include Rack::Request::Env
     include ActionDispatch::Http::Parameters
-    attr_accessor :controller_instance
+    attr_accessor :controller_instance, :path_info
     def initialize(env)
       super
+    end
+    def controller_class
+      return HomeController
+      params = path_parameters
+      if params.key?(:controller)
+        controller_param = params[:controller].underscore
+        params[:action] ||= "index"
+        const_name = "#{controller_param.camelize}Controller"
+        ActiveSupport::Dependencies.constantize(const_name)
+      else
+        #PASS_NOT_FOUND
+      end
     end
   end
   #https://github.com/rails/rails/blob/master/actionpack/lib/action_dispatch/http/response.rb
@@ -164,10 +175,106 @@ module ActionDispatch
       @app, @executor = app, executor
     end
     def call(env)
-      return ['200',[],["hello ","rails from Executor."] ]
-      state = @executor.run! if @executor
+      #return ['200',[],["hello ","rails from Executor."] ]
+      #state = @executor.run! if @executor
       response = @app.call(env)
-      returned = response #<< ::Rack::BodyProxy.new(response.pop) { state.complete! }
+      #returned = response << ::Rack::BodyProxy.new(response.pop) { state.complete! }
+    end
+  end
+
+  module Routing
+    #https://github.com/rails/rails/blob/master/actionpack/lib/action_dispatch/routing/endpoint.rb
+    class Endpoint
+      def app;           self;  end
+    end
+
+    #https://github.com/rails/rails/blob/master/actionpack/lib/action_dispatch/routing/route_set.rb
+    class RouteSet
+      class Dispatcher < Routing::Endpoint
+        def serve(req)
+          return ['200',[],["hello ","rails from RouteSet Dispatcher."] ]
+          params     = req.path_parameters
+          controller = controller req
+          res        = controller.make_response! req
+          dispatch(controller, params[:action], req, res)
+        end
+        def dispatch(controller, action, req, res)
+          controller.dispatch(action, req, res)
+        end
+        def controller(req)
+          req.controller_class
+        end
+      end
+
+      Config = Struct.new :relative_url_root, :api_only
+      DEFAULT_CONFIG = Config.new(nil, false)
+      def self.new_with_config(config)
+        route_set_config = DEFAULT_CONFIG
+        new route_set_config
+      end
+      def initialize(config = DEFAULT_CONFIG)
+        @set    = Journey::Routes.new
+        @router = Journey::Router.new @set
+        #Routes should be set by route mapping registers, for simplicity, hard code here
+        @set.instance_eval{
+          @routes << Dispatcher.new
+        }
+      end
+      def call(env)
+        req = make_request(env)
+        req.path_info='/home/index'
+        req.path_info=''
+        req.path_info = Journey::Router::Utils.normalize_path(req.path_info)
+        @router.serve(req)
+      end
+      def request_class
+        ActionDispatch::Request
+      end
+      def make_request(env)
+        request_class.new env
+      end
+    end
+  end
+
+  module Journey
+    #https://github.com/rails/rails/blob/master/actionpack/lib/action_dispatch/journey/router.rb
+    class Router
+      attr_accessor :routes
+      def initialize(routes)
+        @routes = routes
+      end
+      def serve(req)
+        find_routes(req).each do |match, parameters, route|
+          status, headers, body = route.app.serve(req)
+          return [status, headers, body]
+        end
+        return ['200',[],["hello ","rails from Router 404."] ]
+      end
+      def find_routes(req)
+        routes.map{|x|[nil,nil,x]}
+      end
+      class Utils
+        def self.normalize_path(path)
+          path = "/#{path}"
+          path.squeeze!("/".freeze)
+          path.sub!(%r{/+\Z}, "".freeze)
+          path.gsub!(/(%[a-f0-9]{2})/) { $1.upcase }
+          path = "/" if path == "".freeze
+          path
+        end
+      end
+    end
+
+    #https://github.com/rails/rails/blob/master/actionpack/lib/action_dispatch/journey/routes.rb
+    class Routes
+      include Enumerable
+      attr_reader :routes
+      def initialize
+        @routes             = []
+      end
+      def each(&block)
+        routes.each(&block)
+      end
     end
   end
 end
@@ -245,3 +352,8 @@ module ActionController
 end
 
 
+class HomeController
+  def index
+    ['200',[],["hello ","rails from rails Home Index."]]
+  end
+end
